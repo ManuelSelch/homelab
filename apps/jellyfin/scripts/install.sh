476de0bd524456990323a8e7fb9f2718653b6446
Manuel Selch <manuel@selch.de>
2026-05-14 17:39:31 +0000

    feat(apps): jellyfin

---
+++ apps/jellyfin/scripts/install.sh
@@ -0,0 +1,2 @@
+apt-get install sshfs
+mkdir data/media
---
+++ apps/jellyfin/scripts/mount.sh
@@ -0,0 +1,1 @@
+sshfs -o reconnect,ServerAliveInterval=15,ServerAliveCountMax=3,allow_other,default_permissions,uid=1000,gid=1000 manuelselch@sftp.hidrive.strato.com:/users/manuelselch/music data/media
